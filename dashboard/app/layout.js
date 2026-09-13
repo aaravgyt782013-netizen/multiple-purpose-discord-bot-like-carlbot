5a0757c528478080602853fc8d1fa9477f928596
aaravgyt782013-netizen <aaravgyt782013@gmail.com>
2026-09-14 03:13:42 +0530

    Add dashboard layout

---
+++ dashboard/app/layout.js
@@ -0,0 +1,2 @@
+export const metadata={title:'LightCore Dashboard',description:'Discord bot control center'};
+export default function Layout({children}){return <html lang="en"><body>{children}</body></html>}
