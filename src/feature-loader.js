8cc7189011069ee2e76b260ee6111d00f68da51d
aaravgyt782013-netizen <aaravgyt782013@gmail.com>
2026-09-14 01:29:04 +0530

    Load counter autosetup feature

--- src/feature-loader.js
+++ src/feature-loader.js
@@ -7,6 +7,7 @@ const attachUltimate=require('./ultimate-features.js');
 const attachFinalSuite=require('./final-suite.js');
 const attachMusic=require('./music.js');
 const attachHelpFinal=require('./help-final.js');
+const attachCounterAutosetup=require('./counter-autosetup.js');
 module.exports=function loadFeaturePack(bot){
  if(!bot?.client)throw new Error('LightCore client is unavailable.');
  const dataDir=path.join(process.cwd(),'data');const file=path.join(dataDir,'database.json');
@@ -21,6 +22,7 @@ module.exports=function loadFeaturePack(bot){
  attachUltimate(bot.client,'.');
  attachFinalSuite(bot.client,'.');
  attachMusic(bot.client,'.');
+ attachCounterAutosetup(bot.client,'.');
  attachHelpFinal(bot.client,'.');
  console.log('[FEATURES] Full command stack loaded | prefix: .');
 };
